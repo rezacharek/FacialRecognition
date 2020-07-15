6f668b9ebb2b323f0da36696b10bd2bd80def3dd
Roman Zacharek <zacharekedward@gmail.com>
2020-07-15 19:13:28 +0300

    Showing webcam

--- video_capture.py
+++ video_capture.py
@@ -1,2 +1,19 @@
 import cv2
-import numpy as np
+import numpy as numpy
+
+# Opening the web Cam
+my_web_cam = cv2.VideoCapture(0)
+
+
+c = cv2.waitKey(1)
+scaling_factor = 1
+
+# Capturing new Frames
+while(c != 27):
+    ret, new_frame = my_web_cam.read()
+    new_frame = cv2.resize(new_frame, None, fx=scaling_factor, fy=scaling_factor,
+            interpolation=cv2.INTER_AREA)
+    cv2.imshow('Resized', new_frame)
+    c = cv2.waitKey(1)
+
+cv2.destoryAllWindows()
