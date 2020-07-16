ff76f1972a742714dcda1c6c600715b97a813061
Roman Zacharek <zacharekedward@gmail.com>
2020-07-16 12:05:36 +0300

    File choosing box working completely

--- gui.py
+++ gui.py
@@ -3,6 +3,8 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QFileDialog, QTextEdit, QPus
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import QDir
 
+filenames = []
+
 class DialogApp(QWidget):
 	def __init__(self):
 		super().__init__()
@@ -15,27 +17,18 @@ class DialogApp(QWidget):
 		layout.addWidget(self.button2)
 		self.setLayout(layout)
 
-
 	def get_text_file(self):
 		dialog = QFileDialog()
 		dialog.setFileMode(QFileDialog.AnyFile)
 		dialog.setFilter(QDir.Files)
-
+		self.filenames = []
 		if dialog.exec_():
-			file_name = dialog.selectedFiles()
-            # print(file_name)
-			# if file_name[0].endswith('.py'):
-    		# 		with open(file_name[0], 'r') as f:
-			# 		data = f.read()
-			# 		self.textEditor.setPlainText(data)
-			# 		f.close()
-			# else:
-			# 	pass
+			self.filenames.append(dialog.selectedFiles()[0])
+			print(self.filenames)
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 
 	demo = DialogApp()
 	demo.show()
-
 	sys.exit(app.exec_())
